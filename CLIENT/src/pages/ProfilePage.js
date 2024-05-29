574fc0eddfa9d5d3453239ef77535fde3a81dc44
midhun-sadanand <128321084+midhun-sadanand@users.noreply.github.com>
2024-05-29 16:17:21 -0500

    Bringing main origin up to date

--- CLIENT/src/pages/ProfilePage.js
+++ CLIENT/src/pages/ProfilePage.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { supabase } from '../supabaseClient';
-import ProfileHeader from '../components/common/ProfileHeader';
+import WatchlistPage from './WatchlistPage';
 import FriendsBar from '../components/common/FriendsBar';
 import FriendRequestsDropdown from '../components/common/FriendRequests';
+
+
 
 
 const ProfilePage = () => {
@@ -11,23 +13,30 @@ const ProfilePage = () => {
     const [username, setUsername] = useState('Guest');
     const [loading, setLoading] = useState(true);
     const [userId, setUserId] = useState(null);
+    const [toggle, setToggle] = useState(1);
+    const [friends, setFriends] = useState([]);
+
 
     useEffect(() => {
         const fetchUserData = async () => {
             const { data: { user } } = await supabase.auth.getUser();
+
 
             if (!user) {
                 navigate('/');
                 return;
             }
 
+
             setUserId(user.id);
+
 
             const { data: profile, error } = await supabase
                 .from('profiles')
                 .select('username')
                 .eq('user_id', user.id)
                 .single();
+
 
             if (error) {
                 console.error('Error fetching user data:', error.message);
@@ -36,28 +45,88 @@ const ProfilePage = () => {
                 setUsername(profile.username);
             }
 
+
             setLoading(false);
         };
 
+
+        const fetchFriends = async () => {
+            const { data, error } = await supabase
+                .from('friends')
+                .select(`
+                    friend_id,
+                    friend_profile:friend_id (username) // Adjusted for correct relationship fetching
+                `)
+                .eq('user_id', userId)
+                .eq('status', 'accepted');
+       
+            if (error) {
+                console.error('Error fetching friends:', error.message);
+            } else {
+                console.log('Fetched friends data:', data); // Logs successful data fetch.
+   
+                setFriends(data.map(friend => ({
+                    id: friend.friend_id,
+                    username: friend.friend_profile.username  // Assuming the returned data structure from the query
+                })));
+            }
+        };
+
+
         fetchUserData();
-    }, [navigate]);
+
+
+        if (!userId) return;  // Only fetch if userId is available
+        fetchFriends();
+    }, [navigate, userId]);
+
 
     if (loading) {
         return <div>Loading...</div>;
     }
 
+
+    const updateToggle = (id) => {
+        setToggle(id);
+    }
+
+
+
+
     return (
-        <div className="w-full h-screen flex flex-col items-center justify-start pt-20">
-            <h1 className="text-2xl text-center">
+        <div className="w-screen h-screen flex flex-col items-center pt-20">
+            <h1 className="text-2xl text-center mb-10">
                 Welcome back, <span className="underline">{username}</span>
-
-
-                <FriendsBar userId={userId} />
-                <FriendRequestsDropdown userId={userId} />
-
             </h1>
+            <div className="w-3/4">
+                <ul className="tab-bar flex justify-center border-2 bg-gray-300">
+                    <li className="px-4 py-2 cursor-pointer hover:bg-gray-500" onClick={()=>updateToggle(1)}>Add Item</li>
+                    <li className="px-7 py-2 cursor-pointer hover:bg-gray-500" onClick={()=>updateToggle(2)}>Lists</li>
+                    <li className="px-4 py-2 cursor-pointer hover:bg-gray-500" onClick={()=>updateToggle(3)}>Friends</li>
+                </ul>
+                <div className={toggle === 1 ? "show" : "hidden"}>
+                    <h1>Add media:</h1>
+                    <p>this is where you add media!</p>
+                </div>
+                <div className={toggle === 2 ? "show" : "hidden"}>
+                    <WatchlistPage />
+                </div>
+                <div className={toggle === 3 ? "grid grid-cols-2" : "hidden"}>
+                    <div className="h-screen border-r-2">
+                        <div className="m-6"><FriendsBar userId={userId}/></div>
+                        <FriendRequestsDropdown userId={userId}/>
+                    </div>
+                    <div>
+                    <h2 className="p-2 mt-4">Friends: </h2>
+                    {friends.map(friend => (
+                    <p>{friend.username}</p>
+                ))}
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
+
 
 export default ProfilePage;
--- CLIENT/src/pages/WatchlistPage.js
+++ CLIENT/src/pages/WatchlistPage.js
@@ -2,26 +2,31 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { supabase } from '../supabaseClient';
 
+
 const WatchlistPage = () => {
     const [watchlists, setWatchlists] = useState([]);
     const [watchlistName, setWatchlistName] = useState('');
     const [username, setUsername] = useState('');
     const navigate = useNavigate();
 
+
     useEffect(() => {
         const fetchUserData = async () => {
             const { data: { user } } = await supabase.auth.getUser();
+
 
             if (!user) {
                 navigate('/login');
                 return;
             }
 
+
             const { data: profile, error } = await supabase
                 .from('profiles')
                 .select('username')
                 .eq('user_id', user.id)
                 .single();
+
 
             if (error) {
                 console.error('Error fetching user data:', error.message);
@@ -32,8 +37,10 @@ const WatchlistPage = () => {
             }
         };
 
+
         fetchUserData();
     }, [navigate]);
+
 
     const fetchWatchlists = async (userId) => {
         // Query to fetch watchlists that the user owns
@@ -41,7 +48,7 @@ const WatchlistPage = () => {
             .from('watchlists')
             .select('*')
             .eq('user_id', userId);
-    
+   
         // Query to fetch watchlists that are shared with the user
         const sharedWatchlistsQuery = supabase
             .from('watchlist_shares')
@@ -50,11 +57,11 @@ const WatchlistPage = () => {
                 watchlist:watchlist_id (id, name, user_id)  // Assuming a foreign key from watchlist_shares to watchlists
             `)
             .eq('shared_with_user_id', userId);
-    
+   
         try {
             let { data: ownWatchlists, error: ownError } = await ownWatchlistsQuery;
             let { data: sharedWatchlistsData, error: sharedError } = await sharedWatchlistsQuery;
-    
+   
             if (ownError) {
                 console.error('Error fetching own watchlists:', ownError.message);
                 return;
@@ -63,10 +70,10 @@ const WatchlistPage = () => {
                 console.error('Error fetching shared watchlists:', sharedError.message);
                 return;
             }
-    
+   
             // Map shared watchlists to have the same structure as own watchlists
             const sharedWatchlists = sharedWatchlistsData.map(item => item.watchlist);
-    
+   
             // Combine own and shared watchlists
             const combinedWatchlists = [...ownWatchlists, ...sharedWatchlists];
             setWatchlists(combinedWatchlists);
@@ -74,15 +81,18 @@ const WatchlistPage = () => {
             console.error('Error fetching watchlists:', error.message);
         }
     };
-    
+   
+
 
     const createWatchlist = async () => {
         if (!watchlistName) return;
+
 
         const { data: { user } } = await supabase.auth.getUser();
         const { data, error } = await supabase
             .from('watchlists')
             .insert([{ name: watchlistName, user_id: user.id }]);
+
 
         if (error) {
             console.error('Error creating watchlist:', error.message);
@@ -93,30 +103,50 @@ const WatchlistPage = () => {
         }
     };
 
+
+    const deleteWatchlist = async (deletedId) => {
+        if (window.confirm(`Are you sure you want to delete this list?`)) {
+            const { error } = await supabase
+                .from('watchlists')
+                .delete()
+                .match({ id: deletedId });
+
+
+            if (error) {
+                console.error('Error deleting list:', error.message);
+            } else {
+                setWatchlists(currentWatchlists => currentWatchlists.filter(list => list.id !== deletedId));
+            }
+        }
+    };
+
+
     return (
-        <div className="container mx-auto p-4">
-            <h1 className="text-xl font-bold">Your Watchlists</h1>
-            {watchlists.map((list) => (
-                <div key={list.id} className="p-2 border-b">
-                    <button className="text-blue-500 hover:text-blue-600" onClick={() => navigate(`/list/${username}/${list.name}`)}>
-                        {list.name}
-                    </button>
-                </div>
-            ))}
-            <div>
+        <div className="container mx-auto p-4 grid grid-cols-3">
+            <h1 className="text-xl font-bold col-span-3">Your Watchlists</h1>
+            <div className="col-span-3 flex justify-end">
                 <input
                     type="text"
                     value={watchlistName}
                     onChange={(e) => setWatchlistName(e.target.value)}
                     placeholder="Enter Watchlist Name"
-                    className="border p-2 mt-4"
+                    className="border p-2 my-4"
                 />
-                <button onClick={createWatchlist} className="ml-2 bg-blue-500 text-white p-2 rounded">
+                <button onClick={() => createWatchlist()} className="ml-2 my-4 bg-blue-500 text-white p-2 rounded">
                     Create Watchlist
                 </button>
             </div>
+            {watchlists.map((list) => (
+                <div key={list.id} className="p-2 m-4 border-2">
+                    <button className="text-blue-500 hover:text-blue-600" onClick={() => navigate(`/list/${username}/${list.name}`)}>
+                        {list.name}
+                    </button>
+                    <button className="block hover:underline" onClick={() => deleteWatchlist(list.id)}>remove</button>
+                </div>
+            ))}
         </div>
     );
 };
+
 
 export default WatchlistPage;
