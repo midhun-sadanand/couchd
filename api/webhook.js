27130aa1975ff7edca4094edfaa6ba6edd6e0e73
midhun-sadanand <128321084+midhun-sadanand@users.noreply.github.com>
2024-06-02 00:03:51 -0500

    Restructured API file tree

---
+++ api/webhook.js
@@ -0,0 +1,33 @@
+// api/webhook.js
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    try {
+      const { data: { id: clerkId, emailAddress, firstName, lastName } } = req.body;
+
+      const { data, error } = await supabase
+        .from('profiles')
+        .insert([
+          { clerk_id: clerkId, email: emailAddress, username: `${firstName} ${lastName}` }
+        ]);
+
+      if (error) {
+        console.error('Error inserting data:', error);
+        return res.status(500).json({ error: 'Failed to insert data into Supabase' });
+      }
+
+      return res.status(200).json({ data: 'Profile created successfully' });
+    } catch (error) {
+      console.error('Error handling request:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  } else {
+    res.setHeader('Allow', ['POST']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
